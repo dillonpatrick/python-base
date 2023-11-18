7c23991c2ea881c1d6d5d2dc354fe1930f4f2ea5
dillonpatrick <dillon_patrick@outlook.com>
2023-11-18 12:04:49 -0300

    Validando condições

--- hello.py
+++ hello.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
-print("Hello World")
+"""Comentarios multilinha
+Geralmente usados para dar instruções de execução do codigo
+"""
+__version__ = "0.0.1"
+__author__ = "Dillon Patrick"
+__licence__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "us_US")[:5]
+
+msg = "Hello World"
+
+if current_language == "pt_BR":
+    msg = "Ola Mundo"
+elif current_language == "it_IT":
+    msg = "Ciao Mondo"
+
+print(msg)
