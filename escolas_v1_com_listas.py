0b9acc39da431573389d19afe5aaedd087b1883a
dillonpatrick <dillon_patrick@outlook.com>
2023-11-25 11:43:56 -0300

    update

--- escolas_v1_com_listas.py
+++ escolas_v1_com_listas.py
@@ -7,13 +7,14 @@ Imprimir a lista de crianças agrupadas por sala que frequentam cada uma das ati
 
 __version__ = "0.1.0"
 
-sala1 = ["Dillon", "Patrick", "Jhow", "Allan"]
-sala2 = ["Celina", "Alanna", "Amanda", "Kethelin"]
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
 
-aula_ingles = ["Celina", "Dillon", "Alanna"]
-aula_musica = ["Patrick", "Jhow", "Amanda"]
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
 
-atividades = [("Ingles", aula_ingles), ("Musica", aula_musica)]
+atividades = [("Ingles", aula_ingles), ("Musica", aula_musica), ("Dança", aula_danca)]
 
 for nome_atividade, atividade in atividades:
     atividade_sala1 = []
@@ -24,7 +25,7 @@ for nome_atividade, atividade in atividades:
             atividade_sala1.append(aluno)
         elif aluno in sala2:
             atividade_sala2.append(aluno)
-
+    print("-" * 50)
     print(f"| Alunos da sala 1 em {nome_atividade}: {atividade_sala1}")
     print(f"| Alunos da sala 2 em {nome_atividade}: {atividade_sala2}")
     print("-" * 50)
