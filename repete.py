7b82bca6db0af2f8cdf2140508b3e38dd3259db3
dillon <dillon_patrick@outlook.com>
2024-01-11 12:38:01 -0300

    Testando o break

--- repete.py
+++ repete.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 
-numbers = range(1, 101)
-
-for number in numbers:
-    par = number % 2 == 0
-    if par:
-        print(number)
-    else:
-        continue
-    print("mais codigo", number)
+dados = {}
+for line in open("post.txt"):
+    if "---" in line:
+        break
+    key, value = line.split(":")
+    dados[key] = value.strip()
+print(dados)
